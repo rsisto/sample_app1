b383685fd69633b13deabe6e1f54d93f4b6eb077
Rafael Sisto <rafael.sisto@gmail.com>
2011-02-10 13:49:25 -0200

    punto 10.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
 	@title = @user.name
   end
   
   def edit
-    @user = User.find(params[:id])
 	@title = "Edit user"
   end  
 
@@ -26,6 +28,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 
 
